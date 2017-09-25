efd049c65e6a5cee14cedae074324efa9701c810
Nicholas Bishop <nbishop@neverware.com>
2017-09-25 13:11:00 -0400

    Add infra

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Neverware Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=missing-docstring
+
+import setuptools
+
+setuptools.setup(
+    name='numeric_version',
+    version='1.0.0',
+    description='Very simple module with a numeric version class',
+    author='Nicholas Bishop',
+    author_email='nicholasbishop@gmail.com',
+    license='Apache 2.0',
+    packages=['numeric_version'])
